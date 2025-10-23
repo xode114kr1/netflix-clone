db0ff26f99f74f5016307da45957ee6de376c2fc
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-24 01:42:45 +0900

    feat : tmdb.ts IMovie interface 정의

--- src/models/tmdb.ts
+++ src/models/tmdb.ts
@@ -76,3 +76,23 @@ export interface IMovieSummary {
   vote_average: number;
   vote_count: number;
 }
+
+export interface IMovie {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  origin_country?: string[];
+  original_language: string;
+  original_name?: string;
+  original_title?: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  first_air_date?: string;
+  release_date?: string;
+  name?: string;
+  title?: string;
+  vote_average: number;
+  vote_count: number;
+}
