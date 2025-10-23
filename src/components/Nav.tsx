aab65dfbf303c10f8af616555a79d8e124579136
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-23 21:17:11 +0900

    feat : Nav UI 구현

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,66 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+interface NavContainerProps {
+  $show: boolean;
+}
+
+const NavContanier = styled.nav<NavContainerProps>`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 30px;
+  z-index: 1;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  transition: all 0.5s ease-in;
+
+  background-color: ${(props) => (props.$show ? '#111' : 'transparent')};
+`;
+
+const NavLogoImg = styled.img`
+  position: fixed;
+  left: 40px;
+  width: 80px;
+  object-fit: contain;
+`;
+
+const NavUserImg = styled.img`
+  position: fixed;
+  right: 40px;
+  width: 30px;
+  object-fit: contain;
+`;
+
 export default function Nav() {
-  return <div>Nav</div>;
+  const [show, setShow] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    };
+  }, []);
+
+  return (
+    <NavContanier $show={show}>
+      <NavLogoImg
+        alt="Netflix Logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
+        onClick={() => window.location.reload()}
+      />
+      <NavUserImg
+        alt="User logged"
+        src="https://png.pngtree.com/png-vector/20191115/ourmid/pngtree-beautiful-profile-line-vector-icon-png-image_1990469.jpg"
+      />
+    </NavContanier>
+  );
 }
