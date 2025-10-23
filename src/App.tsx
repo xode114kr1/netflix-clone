a6cc49c821eb289ed03f7b81d7c3bb182c70a6bf
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-24 01:51:10 +0900

    feat : App.tsx에서 Row 컨포넌트 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import requests from './api/request';
 import './App.css';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from './components/Row';
 
 function App() {
   return (
     <>
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trendin Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Acion Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </>
   );
 }
