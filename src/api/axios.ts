fa4f72da82d48bafbb05f867a40e155f51c9e721
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-23 17:44:01 +0900

    feat : axios 인스턴스 생성

---
+++ src/api/axios.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const instance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: API_KEY,
+    language: 'ko-KR',
+  },
+});
+
+export default instance;
