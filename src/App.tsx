e267a4dcbb886788e99cc4008cb8801943fcd274
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-24 00:24:48 +0900

    Nav Banner 컨포넌트 app에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <Nav />
+      <Banner />
+    </>
+  );
 }
 
 export default App;
