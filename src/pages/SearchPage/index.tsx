8682a659eaf25f1cb0da977442da016b7f51bc33
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-24 19:29:37 +0900

    feat : SearchPage api 연동 기능 구현

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,3 +1,35 @@
+import { useLocation } from 'react-router-dom';
+import instance from '../../api/axios';
+import { useEffect, useState } from 'react';
+import type { IMovie } from '../../models/tmdb';
+
 export default function SearchPage() {
+  const [searchResults, setSearchResults] = useState<IMovie[]>([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+  let query = useQuery();
+
+  const searchTerm = query.get('q');
+
+  const fetchSearchMovie = async (searchTerm: string) => {
+    try {
+      const res = await instance.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResults(res.data.results);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  console.log(searchResults);
   return <div>SearchPage</div>;
 }
