9246ced57a1411bdbb8bdcac271387888bd6eec2
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-24 22:26:51 +0900

    feat : DetailPage UI 구현
    feat : API 연동

--- src/pages/DetailPage/index.tsx
+++ src/pages/DetailPage/index.tsx
@@ -1,3 +1,62 @@
+import { useParams } from 'react-router-dom';
+import instance from '../../api/axios';
+import { useEffect, useState } from 'react';
+import type { IMovie } from '../../models/tmdb';
+import styled from 'styled-components';
+
+const DetailContanier = styled.section`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  color: #fff;
+  overflow: hidden;
+`;
+
+const MovieImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  opacity: 0.5;
+`;
+
+export const MovieTitle = styled.h1`
+  position: absolute;
+  bottom: 80px;
+  left: 60px;
+  font-size: 3rem;
+  font-weight: bold;
+  text-shadow: 0px 4px 10px rgba(0, 0, 0, 0.7);
+
+  @media screen and (max-width: 768px) {
+    font-size: 2rem;
+    left: 30px;
+    bottom: 60px;
+  }
+`;
+
 export default function DetailPage() {
-  return <div>DetailPage</div>;
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState<IMovie | null>(null);
+  useEffect(() => {
+    fetchMovieDetail();
+  }, [movieId]);
+
+  const fetchMovieDetail = async () => {
+    try {
+      const res = await instance.get(`/movie/${movieId}`);
+      setMovie(res.data);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  return (
+    <DetailContanier>
+      <MovieImage
+        src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+        alt="poster"
+      />
+      <MovieTitle>{movie?.title}</MovieTitle>
+    </DetailContanier>
+  );
 }
