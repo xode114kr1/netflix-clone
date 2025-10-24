0a5df123b5ab54f77fe4c4468ccabdc36cc29f0f
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-24 18:12:03 +0900

    feat : App.tsx에 footer 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import requests from './api/request';
 import './App.css';
 import Banner from './components/Banner';
+import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Row from './components/Row';
 
@@ -23,6 +24,7 @@ function App() {
         id="CM"
         fetchUrl={requests.fetchComedyMovies}
       />
+      <Footer />
     </>
   );
 }
