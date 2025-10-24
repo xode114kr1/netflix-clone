ba7163253d8c54ef8ea2524c4fe9a992f5f65790
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-24 18:57:18 +0900

    feat : MainPage, DetailPage, SearchPage에 대한 라우팅을 App.ts에 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-import requests from './api/request';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
-import Row from './components/Row';
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
+import SearchPage from './pages/SearchPage';
+
+const Layout = () => {
+  return (
+    <>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 function App() {
   return (
     <>
-      <Nav />
-      <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        id="NO"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trendin Now" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row title="Acion Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
-      <Row
-        title="Comedy Movies"
-        id="CM"
-        fetchUrl={requests.fetchComedyMovies}
-      />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,3 +1,25 @@
+import requests from '../../api/request';
+import Banner from '../../components/Banner';
+import Row from '../../components/Row';
+
 export default function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <>
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trendin Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Acion Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+    </>
+  );
 }
