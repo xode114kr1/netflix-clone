f6a4b34883fed067a238a61db5f338d30d5926f2
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-24 19:13:44 +0900

    fix : footer의 z-index nav보다 작은 숫자로 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const FooterContainer = styled.div`
   border-top: 1px solid rgb(25, 25, 25);
   width: 100%;
   position: relative;
-  z-index: 100;
+  z-index: 1;
 
   @media (max-width: 769px) {
     padding: 20px 20px;
